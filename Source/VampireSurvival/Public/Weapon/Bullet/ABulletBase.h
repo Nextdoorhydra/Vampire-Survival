497f886983aabd2100ea6e3c2c2e8893e0768b4e
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-11 16:55:46 +0900

    feat : 무기 / 총알 의 추상 클래스 정의  - Simple Bullet 클래스 구현

---
+++ Source/VampireSurvival/Public/Weapon/Bullet/ABulletBase.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ABulletBase.generated.h"
+
+UCLASS(Abstract)
+class VAMPIRESURVIVAL_API ABulletBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABulletBase();
+
+	virtual void BulletSpawn(AActor* InOwner, FVector InDirection, float Damage){}
+	
+protected:
+	//life time
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<USceneComponent> DefaultSceneRoot;
+};
