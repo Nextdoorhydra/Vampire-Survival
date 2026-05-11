0e058709610605a149c10428883f9b3ad6cf4595
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-11 16:50:29 +0900

    feat : Hitable 컴포넌트 구현, Hitable 인터페이스 정의  - 컴포넌트는 OnHPChanged, OnDeath 델리게이트를 가짐

---
+++ Source/VampireSurvival/Private/Hitable/HitableComponent.cpp
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Hitable/HitableComponent.h"
+
+
+// Sets default values for this component's properties
+UHitableComponent::UHitableComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UHitableComponent::Initialize(float InMaxHP)
+{
+	SetMaxHP(InMaxHP);
+	CurrentHP = InMaxHP;
+}
+
+void UHitableComponent::SetMaxHP(float Amount)
+{
+	MaxHP = Amount; 
+	OnHPChanged();
+}
+
+//TODO 최대체력 오를때, 현재 체력도 같이 올라야하나?
+void UHitableComponent::AddMaxHP(float Amount)
+{
+	MaxHP += Amount; 
+	OnHPChanged();
+}
+
+
+void UHitableComponent::SetHP(float Amount)
+{
+	CurrentHP = Amount;
+	OnHPChanged();
+}
+
+void UHitableComponent::AddHP(float Amount)
+{
+	CurrentHP -= Amount;
+	OnHPChanged();
+}
+
+void UHitableComponent::OnHPChanged()
+{
+	OnHPChangedEvent.Broadcast(CurrentHP, MaxHP);
+	if (CurrentHP > MaxHP) CurrentHP = MaxHP;
+	if (CurrentHP <= 0.0f) OnDeathEvent.Broadcast();
+}
+
+
+
+
---
+++ Source/VampireSurvival/Public/Hitable/HitableComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HitableComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHPChangedDelegate, float, CurrentHP, float, MaxHP);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDeathDelegate);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class VAMPIRESURVIVAL_API UHitableComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UHitableComponent();
+	
+	virtual void Initialize(float InMaxHP);
+	
+	//getter setter
+	float GetMaxHP() const { return MaxHP; }
+	void SetMaxHP(float Amount);
+	virtual void AddMaxHP(float Amount);
+	float GetHP() const { return CurrentHP; }
+	void SetHP(float Amount);
+	void AddHP(float Amount);
+	
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnHPChangedDelegate OnHPChangedEvent; // 체력 변경시 호출될 이벤트
+	
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnDeathDelegate OnDeathEvent; // 사망 시 호출될 이벤트
+	
+protected:
+	void OnHPChanged();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy|Stats")
+	float MaxHP = 100.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Enemy|Stats")
+	float CurrentHP = 100.f;
+};
