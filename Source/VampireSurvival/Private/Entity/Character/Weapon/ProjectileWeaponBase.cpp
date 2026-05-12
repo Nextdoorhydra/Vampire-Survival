59f785d5f56c8d1d72fe2bdecc71f44949cf56fd
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-12 14:02:13 +0900

    feat : 투척 무기 베이스 클래스 추가 & 투척 단검 무기 추가  - 단검 무기 동작 구현

---
+++ Source/VampireSurvival/Private/Entity/Character/Weapon/ProjectileWeaponBase.cpp
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Entity/Character/Weapon/ProjectileWeaponBase.h"
+#include "Entity/Character/Weapon/Bullet/ABulletBase.h"
+
+
+// Sets default values
+AProjectileWeaponBase::AProjectileWeaponBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AProjectileWeaponBase::RequestBulletSpawn(FTransform SpawnTransform)
+{
+	//TODO 오브젝트 풀링 추가
+	if (!BulletClass)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("BulletClass가 지정되지 않았습니다!"));
+		return;
+	}
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;                         // 이 무기가 주인임
+	SpawnParams.Instigator = GetInstigator();         // 무기를 든 캐릭터가 가해자임
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn; // 일단 무조건 스폰
+
+	ABulletBase* NewBullet = GetWorld()->SpawnActor<ABulletBase>(BulletClass, SpawnTransform);
+
+	NewBullet->BulletSpawn(this, Damage, KnockBack, 1.f);
+}
+
+
---
+++ Source/VampireSurvival/Public/Entity/Character/Weapon/ThrowingDagger.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectileWeaponBase.h"
+#include "ThrowingDagger.generated.h"
+
+class UCurveVector;
+class UCurveFloat;
+
+UCLASS()
+class VAMPIRESURVIVAL_API AThrowingDagger : public AProjectileWeaponBase
+{
+	GENERATED_BODY()
+	
+public:
+	AThrowingDagger();
+
+protected:
+	virtual void OnAttacking() override;
+
+	UPROPERTY(EditAnywhere, Category = "Weapon|Spawn")
+	TObjectPtr<UCurveVector> SpawnLocationCurve;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon|Spawn")
+	TObjectPtr<UCurveFloat> SpawnTimingCurve;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon|Stats")
+	int32 DaggerCount = 1;
+
+private:
+	FTransform GetRandomSpawnTransform() const;
+};
