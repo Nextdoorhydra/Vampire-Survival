e02358740595506689763da5b8852ca86e515d61
NDH <ndhphysics@gmail.com>
2026-05-12 14:38:24 +0900

    feat: add UIOption - UIMainMenu를 경유하는 팝업

---
+++ Source/VampireSurvival/Private/UI/UIOption.cpp
@@ -0,0 +1,16 @@
+﻿#include "UI/UIOption.h"
+#include "Components/Button.h"
+
+void UUIOption::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	if (Btn_Close)
+	{
+		Btn_Close->OnClicked.AddDynamic(this, &UUIOption::OnCloseClicked);
+	}
+}
+
+void UUIOption::OnCloseClicked()
+{
+	CloseUI();
+}
