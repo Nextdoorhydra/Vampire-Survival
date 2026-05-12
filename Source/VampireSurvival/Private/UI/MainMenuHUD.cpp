230820d8b8e1a77dff626a9e5b0e543345752657
NDH <ndhphysics@gmail.com>
2026-05-12 14:37:58 +0900

    feat: add MainMenuHUD - MainMenuHUD는 레벨 로드 시에 UI 화면에 표시 담당 액터

---
+++ Source/VampireSurvival/Private/UI/MainMenuHUD.cpp
@@ -0,0 +1,30 @@
+﻿#include "UI/MainMenuHUD.h"
+#include "UI/UIRoot.h"
+#include "UI/UIManager.h"
+#include "Engine/LocalPlayer.h"
+
+void AMainMenuHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PC = GetOwningPlayerController())
+	{
+		if (TitleRootClass)
+		{
+			UUIRoot* RootWidget = CreateWidget<UUIRoot>(PC, TitleRootClass);
+			
+			if (RootWidget)
+			{
+				RootWidget->AddToViewport();
+				
+				if (ULocalPlayer* LP = PC->GetLocalPlayer())
+				{
+					if (UUIManager* UIManager = LP->GetSubsystem<UUIManager>())
+					{
+						UIManager->PushWidget(MenuLayerTag, MainMenuClass);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Source/VampireSurvival/Public/UI/MainMenuHUD.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "GameplayTagContainer.h"
+#include "MainMenuHUD.generated.h"
+
+class UUIRoot;
+class UCommonActivatableWidget;
+
+UCLASS()
+class VAMPIRESURVIVAL_API AMainMenuHUD : public AHUD
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+
+	// 블루프린트에서 WBP_TitleRoot 할당
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Classes")
+	TSubclassOf<UUIRoot> TitleRootClass;
+
+	// 블루프린트에서 WBP_MainMenu 할당
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Classes")
+	TSubclassOf<UCommonActivatableWidget> MainMenuClass;
+
+	// 메인 메뉴를 띄울 대상 레이어 태그 (UI.Layer.Menu)
+	UPROPERTY(EditDefaultsOnly, Category = "UI|Tags")
+	FGameplayTag MenuLayerTag;
+};
