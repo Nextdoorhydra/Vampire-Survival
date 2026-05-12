6ccec4d793cb55f5933539f28c1352a14fbb484a
이정준 <53210642+MurRuck@users.noreply.github.com>
2026-05-12 15:33:24 +0900

    feat: 아이템 드랍 & 경험치 구슬 구현

---
+++ Source/VampireSurvival/Private/Item/DropComponents.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item/DropComponents.h"
+
+// Sets default values for this component's properties
+UDropComponents::UDropComponents()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UDropComponents::TryDropItem()
+{
+	if (dropItemClass == nullptr)
+	{
+		return;
+	}
+
+	AActor* Owner = GetOwner();
+
+	if (Owner == nullptr)
+	{
+		return;
+	}
+
+	float RandomValue = FMath::FRand();
+
+	if (RandomValue > dropChance)
+	{
+		return;
+	}
+
+	FVector spawnLocation = Owner->GetActorLocation();
+	FRotator spawnRotation = FRotator::ZeroRotator;
+
+	GetWorld()->SpawnActor<AActor>(
+		dropItemClass,
+		spawnLocation,
+		spawnRotation
+	);
+
+	UE_LOG(LogTemp, Warning, TEXT("Item Dropped"));
+}
+
+
