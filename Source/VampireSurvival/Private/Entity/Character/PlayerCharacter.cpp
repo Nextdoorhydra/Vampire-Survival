fcb5b9298a982a33ae307df01c0055db2d0244d7
DESKTOP-JV9Q3R0\y8268 <y826855@naver.com>
2026-05-12 09:28:35 +0900

    feat : 플레이어 데이터 클래스 추가  - 플레이어 경험치 획득 & 레벨업 함수 준비

--- Source/VampireSurvival/Private/Entity/Character/PlayerCharacter.cpp
+++ Source/VampireSurvival/Private/Entity/Character/PlayerCharacter.cpp
@@ -22,3 +22,23 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
+
+
+void APlayerCharacter::AddExp(float InExp)
+{
+	PlayerData.Exp += InExp;
+	OnExpChangedDelegate.Broadcast(PlayerData.Exp, MaxExp);
+	if (PlayerData.Exp < MaxExp) return;
+
+	//경험치 한번에 많이 들어왔을때 로직 수정 필요
+	
+	LevelUp();
+	PlayerData.Exp -= MaxExp;
+	OnExpChangedDelegate.Broadcast(PlayerData.Exp, MaxExp);
+}
+
+void APlayerCharacter::LevelUp()
+{
+	PlayerData.Level += 1;
+	OnLevelUpDelegate.Broadcast(PlayerData.Level);
+}
