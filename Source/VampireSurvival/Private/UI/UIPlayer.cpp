682ef3fb76310ba404c4eaee9865cb62c599b363
NDH <ndhphysics@gmail.com>
2026-05-13 19:52:54 +0900

    feat: UIPlayer

---
+++ Source/VampireSurvival/Private/UI/UIPlayer.cpp
@@ -0,0 +1,61 @@
+﻿#include "UI/UIPlayer.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+
+void UUIPlayer::OnHealthChanged(float CurrentHealth, float MaxHealth)
+{
+	if (PB_Health && MaxHealth > 0.f)
+	{
+		// 0.0 ~ 1.0 사이의 비율로 프로그레스 바 업데이트
+		PB_Health->SetPercent(CurrentHealth / MaxHealth);
+	}
+}
+
+void UUIPlayer::OnExpChanged(float CurrentExp, float MaxExp)
+{	
+	if (PB_Exp && MaxExp > 0.f)
+	{
+		PB_Exp->SetPercent(CurrentExp / MaxExp);
+	}
+}
+
+void UUIPlayer::OnLevelChanged(int32 NewLevel)
+{
+	if (Text_Level)
+	{
+		FText FormattedGold = FText::Format(FText::FromString(TEXT("Lv: {0}")), FText::AsNumber(NewLevel));
+		Text_Gold->SetText(FormattedGold);
+	}
+}
+
+void UUIPlayer::OnGoldChanged(int32 NewGold)
+{
+	if (Text_Gold)
+	{
+		FText FormattedGold = FText::Format(FText::FromString(TEXT("Gold: {0}")), FText::AsNumber(NewGold));
+		Text_Gold->SetText(FormattedGold);
+	}
+}
+
+void UUIPlayer::OnKillChanged(int32 NewKill)
+{
+	if (Text_Kill)
+	{
+		FText FormattedKill = FText::Format(FText::FromString(TEXT("Kills: {0}")), FText::AsNumber(NewKill));
+		Text_Kill->SetText(FormattedKill);
+	}
+}
+
+void UUIPlayer::OnTimerChanged(float NewTime)
+{
+	if (Text_Timer)
+	{
+		int32 TotalSeconds = FMath::FloorToInt(NewTime); // 소수점 버림
+		int32 Minutes = TotalSeconds / 60;
+		int32 Seconds = TotalSeconds % 60;
+
+		FString TimeString = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+
+		Text_Timer->SetText(FText::FromString(TimeString));
+	}
+}
---
+++ Source/VampireSurvival/Public/UI/UIPlayer.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/UIBase.h"
+#include "UIPlayer.generated.h"
+
+class UProgressBar;
+class UTextBlock;
+
+UCLASS()
+class VAMPIRESURVIVAL_API UUIPlayer : public UUIBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION()
+	void OnHealthChanged(float CurrentHealth, float MaxHealth);
+
+	UFUNCTION()
+	void OnExpChanged(float CurrentExp, float MaxExp);
+
+	UFUNCTION()
+	void OnLevelChanged(int32 NewLevel);
+	
+	UFUNCTION()
+	void OnGoldChanged(int32 NewGold);
+	
+	UFUNCTION()
+	void OnKillChanged(int32 NewKill);
+	
+	UFUNCTION()
+	void OnTimerChanged(float NewTime);
+
+protected:
+	// UMG 에디터에서 동일한 이름으로 위젯을 생성해야 바인딩됩니다.
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UProgressBar* PB_Health;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UProgressBar* PB_Exp;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* Text_Level;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* Text_Gold;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* Text_Kill;
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* Text_Timer;
+};
