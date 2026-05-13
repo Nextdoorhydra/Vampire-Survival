5762a5b0e31249a5deda8a028227dc65183b7bbd
NDH <ndhphysics@gmail.com>
2026-05-13 19:54:38 +0900

    feat: UIClear - There is no implementation

---
+++ Source/VampireSurvival/Private/UI/UIClear.cpp
@@ -0,0 +1,19 @@
+﻿#include "UI/UIClear.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void UUIClear::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (Btn_ReturnToTitle)
+	{
+		Btn_ReturnToTitle->OnClicked.AddDynamic(this, &UUIClear::OnReturnToTitleClicked);
+	}
+}
+
+void UUIClear::OnReturnToTitleClicked()
+{
+	// 타이틀 레벨(메인 메뉴)로 씬 전환
+	UGameplayStatics::OpenLevel(this, FName("MainMenuLevel"));
+}
---
+++ Source/VampireSurvival/Public/UI/UIClear.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UI/UIBase.h"
+#include "UIClear.generated.h"
+
+class UButton;
+
+UCLASS()
+class VAMPIRESURVIVAL_API UUIClear : public UUIBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeOnInitialized() override;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* Btn_ReturnToTitle;
+
+private:	
+	UFUNCTION() // OnClicked가 Dynamic delegate여서 매크로 붙임
+	void OnReturnToTitleClicked();
+};
