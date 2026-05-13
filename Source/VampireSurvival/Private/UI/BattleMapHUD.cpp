5ea25528b3e8dfadfc8361adfa86321f66a7673e
NDH <ndhphysics@gmail.com>
2026-05-13 19:55:48 +0900

    feat: add BattmeMapHUD & BP_BattleMap_UIRoot - VampireSurvival_GameMode에 HUD 변경함

---
+++ Source/VampireSurvival/Private/UI/BattleMapHUD.cpp
@@ -0,0 +1,29 @@
+﻿#include "UI/BattleMapHUD.h"
+#include "UI/UIRoot.h"
+#include "UI/UIManager.h"
+#include "Engine/LocalPlayer.h"
+
+void ABattleMapHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PC = GetOwningPlayerController())
+	{
+		if (BattleRootClass)
+		{
+			UUIRoot* RootWidget = CreateWidget<UUIRoot>(PC, BattleRootClass);
+			if (RootWidget)
+			{
+				RootWidget->AddToViewport();
+
+				if (ULocalPlayer* LP = PC->GetLocalPlayer())
+				{
+					if (UUIManager* UIManager = LP->GetSubsystem<UUIManager>())
+					{
+						UIManager->PushWidget(GameLayerTag, GameClass);
+					}
+				}
+			}
+		}
+	}
+}
